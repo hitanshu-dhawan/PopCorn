d894326e12fb6eadf1bbba4a1e9b947f5876205d
Hitanshu <hitanshudhawan1996@gmail.com>
2017-07-27 01:27:55 +0530

    Initial commit

---
+++ app/src/main/java/com/hitanshudhawan/popcorn/MainActivity.java
@@ -0,0 +1,98 @@
+package com.hitanshudhawan.popcorn;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout mDrawer;
+    NavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(MainActivity.this);
+        mNavigationView.setCheckedItem(R.id.nav_movies);
+        // TODO set movies fragment by default
+    }
+
+    @Override
+    public void onBackPressed() {
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_search:
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawer.closeDrawer(GravityCompat.START);
+
+        switch (id) { // TODO make separte methods for different fragments loading.
+            case R.id.nav_movies:
+                setTitle(R.string.movies);
+                return true;
+            case R.id.nav_tv_shows:
+                setTitle(R.string.tv_shows);
+                return true;
+            case R.id.nav_popular_people:
+                setTitle(R.string.popular_people);
+                return true;
+            case R.id.nav_favorites:
+                setTitle(R.string.favorites);
+                return true;
+            case R.id.nav_offline:
+                setTitle(R.string.offline);
+                return true;
+            case R.id.nav_settings:
+                return false;
+            case R.id.nav_about:
+                return false;
+        }
+
+        return false;
+    }
+}
