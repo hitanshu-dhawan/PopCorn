dca5863937fd8a398d53775fb6be1c934ef56160
Hitanshu <hitanshudhawan1996@gmail.com>
2017-07-30 14:36:25 +0530

    added MovieDetail Activity

---
+++ app/src/main/java/com/hitanshudhawan/popcorn/MovieDetailActivity.java
@@ -0,0 +1,26 @@
+package com.hitanshudhawan.popcorn;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+    }
+}
