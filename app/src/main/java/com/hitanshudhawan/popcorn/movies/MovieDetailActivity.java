84c4446e1d24d72c8de069046b7aa6033c701fe3
Hitanshu <hitanshudhawan1996@gmail.com>
2017-08-02 00:52:42 +0530

    added Title,Genres,Year in MovieDetail Activity

--- app/src/main/java/com/hitanshudhawan/popcorn/movies/MovieDetailActivity.java
+++ app/src/main/java/com/hitanshudhawan/popcorn/movies/MovieDetailActivity.java
@@ -3,12 +3,15 @@ package com.hitanshudhawan.popcorn.movies;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.solver.widgets.ConstraintAnchor;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -17,14 +20,17 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.hitanshudhawan.popcorn.R;
+import com.hitanshudhawan.popcorn.adapters.MoviesBigAdapter;
 import com.hitanshudhawan.popcorn.network.ApiClient;
 import com.hitanshudhawan.popcorn.network.ApiInterface;
+import com.hitanshudhawan.popcorn.network.movies.Genre;
 import com.hitanshudhawan.popcorn.network.movies.Movie;
 import com.wang.avi.AVLoadingIndicatorView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,7 +40,7 @@ public class MovieDetailActivity extends AppCompatActivity {
 
     private int mMovieId;
 
-    private RelativeLayout mMovieTabLayout;
+    private ConstraintLayout mMovieTabLayout;
 
     private ImageView mPosterImageView;
     private int mPosterHeight;
@@ -44,8 +50,12 @@ public class MovieDetailActivity extends AppCompatActivity {
     private int mBackdropHeight;
     private int mBackdropWidth;
     private AVLoadingIndicatorView mBackdropProgressBar;
+    private TextView mTitleTextView;
+    private TextView mGenreTextView;
+    private TextView mYearTextView;
 
     private TextView mOverviewTextView;
+    private LinearLayout mReleaseAndRuntimeTextLayout;
     private TextView mReleaseAndRuntimeTextView;
 
 
@@ -66,8 +76,8 @@ public class MovieDetailActivity extends AppCompatActivity {
         mBackdropWidth = getResources().getDisplayMetrics().widthPixels;
         mBackdropHeight = (int)(mBackdropWidth / 1.77);
 
-        mMovieTabLayout = (RelativeLayout) findViewById(R.id.layout_toolbar_movie);
-        mMovieTabLayout.getLayoutParams().height = mBackdropHeight + (int)(mPosterHeight * 0.8);
+        mMovieTabLayout = (ConstraintLayout) findViewById(R.id.layout_toolbar_movie);
+        mMovieTabLayout.getLayoutParams().height = mBackdropHeight + (int)(mPosterHeight * 0.9);
 
         mPosterImageView = (ImageView) findViewById(R.id.image_view_poster);
         mPosterImageView.getLayoutParams().width = mPosterWidth;
@@ -78,7 +88,12 @@ public class MovieDetailActivity extends AppCompatActivity {
         mBackdropImageView.getLayoutParams().height = mBackdropHeight;
         mBackdropProgressBar = (AVLoadingIndicatorView) findViewById(R.id.progress_bar_backdrop);
 
+        mTitleTextView = (TextView) findViewById(R.id.textview_title_movie_detail);
+        mGenreTextView = (TextView) findViewById(R.id.textview_genre_movie_detail);
+        mYearTextView = (TextView) findViewById(R.id.textview_year_movie_detail);
+
         mOverviewTextView = (TextView) findViewById(R.id.textview_overview_movie_detail);
+        mReleaseAndRuntimeTextLayout = (LinearLayout) findViewById(R.id.layout_release_and_runtime_movie_detail);
         mReleaseAndRuntimeTextView = (TextView) findViewById(R.id.textview_release_and_runtime_movie_detail);
 
         loadActivity();
@@ -128,8 +143,15 @@ public class MovieDetailActivity extends AppCompatActivity {
                         })
                         .into(mBackdropImageView);
 
+                mTitleTextView.setText(response.body().getTitle());
+
+                setGenres(mMovieId, response.body().getGenres());
+
+                setYear(mMovieId, response.body().getReleaseDate());
+
                 mOverviewTextView.setText(response.body().getOverview());
 
+                mReleaseAndRuntimeTextLayout.setVisibility(View.VISIBLE);
                 String releaseAndRuntimeString = "";
                 if(response.body().getReleaseDate() != null) {
                     SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
@@ -160,5 +182,31 @@ public class MovieDetailActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void setGenres(Integer movieId, List<Genre> genresList) {
+        String genres = "";
+        for (int i=0;i<genresList.size();i++) {
+            if(i == genresList.size()-1) {
+                genres = genres.concat(genresList.get(i).getGenreName());
+            }
+            else {
+                genres = genres.concat(genresList.get(i).getGenreName()+", ");
+            }
+        }
+        mGenreTextView.setText(genres);
+    }
+
+    private void setYear(Integer movieId, String releaseDateString) {
+        if(releaseDateString != null) {
+            SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy");
+            try {
+                Date releaseDate = sdf1.parse(releaseDateString);
+                mYearTextView.setText(sdf2.format(releaseDate));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
