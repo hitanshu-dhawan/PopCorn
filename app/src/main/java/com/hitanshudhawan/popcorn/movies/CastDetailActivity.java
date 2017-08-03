b21b5bb17ee1365dafe81fa5bdffbf90801a9dc0
Hitanshu <hitanshudhawan1996@gmail.com>
2017-08-04 00:30:38 +0530

    made header in CastDetail Activity dynamic for all Screen Sizes

--- app/src/main/java/com/hitanshudhawan/popcorn/movies/CastDetailActivity.java
+++ app/src/main/java/com/hitanshudhawan/popcorn/movies/CastDetailActivity.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -37,6 +40,8 @@ public class CastDetailActivity extends AppCompatActivity {
 
     private int mCastId;
 
+    private CardView mCastImageCardView;
+    private int mCastImageSideSize;
     private ImageView mCastImageView;
     private AVLoadingIndicatorView mProgressBar;
     private TextView mCastNameTextView;
@@ -55,9 +60,17 @@ public class CastDetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         mCastId = intent.getIntExtra("cast_id",-1);
 
+        mCastImageSideSize = (int)(getResources().getDisplayMetrics().widthPixels * 0.33);
+
+        mCastImageCardView = (CardView) findViewById(R.id.card_view_cast_detail);
+        mCastImageCardView.getLayoutParams().height = mCastImageSideSize;
+        mCastImageCardView.getLayoutParams().width = mCastImageSideSize;
+        mCastImageCardView.setRadius(mCastImageSideSize/2);
         mCastImageView = (ImageView) findViewById(R.id.image_view_cast_detail);
         mProgressBar = (AVLoadingIndicatorView) findViewById(R.id.progress_bar_cast_detail);
         mCastNameTextView = (TextView) findViewById(R.id.text_view_name_cast_detail);
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) mCastNameTextView.getLayoutParams();
+        params.setMargins(params.leftMargin,mCastImageSideSize/3,params.rightMargin,params.bottomMargin);
         mCastAgeTextView = (TextView) findViewById(R.id.text_view_age_cast_detail);
         mCastBirthPlaceTextView = (TextView) findViewById(R.id.text_view_birthplace_cast_detail);
 
